b6a9fd63dbfcb8a274a3731212e52232622cdf81
palmiv <palmividar17@gmail.com>
2015-01-24 02:02:40 +0000

    nwe program
    hehe

--- swag.py
+++ swag.py
@@ -1,6 +1,9 @@
-print ("me and hinni are gay bois")
-
-#test1
-#test 2
-
-#palmitest123
+nr1 = int(input("Sláðu inn tölu 1: "))
+nr2 = int(input("Sláðu inn tölu 2: "))
+def summa(x, y):
+    while y:
+        x, y = y, x%y
+    return x
+swag = summa(nr1,nr2)
+print ("Stærsti sameiginlegi þáttur ",nr1," og ",nr2," er ",swag)
+ 
